33f0a603ec7e24678190c6b09e0ef7b27d497180
andela-abankole <akinjide.bankole@andela.co>
2015-09-23 20:39:59 +0100

    test (Jasmine Suite): include all test suite

---
+++ jasmine/spec/invertedIndexTest.js
@@ -0,0 +1,13 @@
+describe('invertedIndex', function() {
+  describe('Read book data', function() {
+    
+  });
+
+  describe('Populate Index', function() {
+    
+  });
+
+  describe('Search index', function() {
+    
+  });
+});
