809744fbf0b03ea742d0c69e18277c91c1039ffe
andela-abankole <akinjide.bankole@andela.co>
2015-09-26 14:56:09 +0100

    test (spec): reads the JSON file and asserts that it is not empty

--- jasmine/spec/invertedIndexTest.js
+++ jasmine/spec/invertedIndexTest.js
@@ -1,13 +1,36 @@
-describe('invertedIndex', function() {
+describe('invertedIndex: ', function() {
   describe('Read book data', function() {
+  //var url = "books.json";
+
+  // xmlhttp.onreadystatechange = function() {
+  // if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+  //     var myArr = JSON.parse(xmlhttp.responseText);
+  //     myFunction(myArr);
+  //     }
+  // }
+
+  // xmlhttp.open("GET", url, true);
+  // xmlhttp.send();
+
+  // function myFunction (myArr) {
+  //   // body...
+  //   console.log(myArr);
+  // }
+  
+    it('should read the JSON file', function() {
+      expect(json).toBeDefined();
+    });
+
+    it('should assert that it is not empty', function() {
+      expect(json.length).toBeGreaterThan(1);
+    });
+  });
+
+  xdescribe('Populate Index', function() {
     
   });
 
-  describe('Populate Index', function() {
-    
-  });
-
-  describe('Search index', function() {
+  xdescribe('Search index', function() {
     
   });
 });
