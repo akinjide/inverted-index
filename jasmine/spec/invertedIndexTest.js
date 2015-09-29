5928584cd38e0d59767ab2e68400f21d773bf81e
andela-abankole <akinjide.bankole@andela.co>
2015-09-29 20:32:55 +0100

    test (Suite): add populate index test
    add test for populate index
    test 1: should verify index is created once JSON file has been read
    test 2: should ensure index is correct
    added doc and refactor formatting and missing semi-colons

--- jasmine/spec/invertedIndexTest.js
+++ jasmine/spec/invertedIndexTest.js
@@ -1,9 +1,17 @@
 describe('invertedIndex: ', function() {
-  describe('Read book data', function() {
-    var jsonData;
+  // Global Variables
+  var jsonData  = undefined
+    , url       = "http://localhost:8000/jasmine/books.json"
+    , index     = Index() // create an instance of the Index constructor
+    , bookIndex  = index.createIndex(url); // call createIndex method, *bookIndex* used in suite : Populate Index
 
+  describe('Read book data', function() {
     beforeEach(function() {
-      jsonData = load();
+      /**
+       * [jsonData store load() response from invertedindex script file]
+       * @type {[Object]} [JSON Object]
+       */
+      jsonData = load(url);
     });
 
     it('should read the JSON file', function() {
@@ -14,24 +22,31 @@ describe('invertedIndex: ', function() {
       expect(jsonData.length).toBeGreaterThan(0);
       jsonData.forEach(function(elem, index) {
         expect(elem).toEqual(jasmine.any(Object));
-        console.log(elem, index);
       });
     });
 
     it('should contain a property value string for each object in JSON file', function() {
       jsonData.forEach(function(elem, index) {
-        console.log(elem, index);
         for (var i in elem) {
-          console.log(elem[i]);
           expect(elem[i]).toEqual(jasmine.any(String))
         }
       });
     });
   });
 
-  xdescribe('Populate Index', function() {
-    it('should behave...', function() {
-      expect()
+  describe('Populate Index', function() {
+    it('should verify index is created once JSON file has been read', function() {
+      expect(bookIndex.length).not.toBe(0);
+    });
+
+    it('should ensure index is correct', function() {
+      bookIndex.forEach(function(elem, index) {
+        expect(index).toEqual(jasmine.any(Number));
+      })
+    });
+
+    xit('should verify the index maps the string keys to the correct objects in the JSON array', function() {
+
     });
   });
 
